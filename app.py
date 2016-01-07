d71de00072847252ed9a817aa2dcf939e51fbcac
jc <shinvee@spoqa.com>
2016-01-07 23:40:08 +0900

    Pass flake8

--- app.py
+++ app.py
@@ -5,15 +5,19 @@ from flask import Flask, json, request
 from flask.ext.cache import Cache
 import requests
 
+
 def make_celery(app):
     celery = Celery(app.import_name, broker=app.config['BROKER_URL'])
     celery.conf.update(app.config)
     TaskBase = celery.Task
+
     class ContextTask(TaskBase):
         abstract = True
+
         def __call__(self, *args, **kwargs):
             with app.app_context():
                 return TaskBase.__call__(self, *args, **kwargs)
+
     celery.Task = ContextTask
     return celery
 
